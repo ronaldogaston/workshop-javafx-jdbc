932c106b9a7ab8f3fb43cb831963cd7817fa20c7
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-05-01 14:24:35 -0300

    Implentação Ajuda.FXML

--- src/application/Main.java
+++ src/application/Main.java
@@ -10,6 +10,8 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 	
+	private static Scene mainScene; // Boa prática
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -19,13 +21,17 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true); // Serve para ajustar o MenuBar (Barra de tarefas da parte de cima)
 			scrollPane.setFitToWidth(true); // Serve para ajustar o MenuBar (Barra de tarefas da parte de cima)
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Aplicação JavaFX");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	public static Scene getMainScene() {
+		return mainScene;
 	}
 
 	public static void main(String[] args) {
--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -1,11 +1,20 @@
 package gui;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import application.Main;
+import gui.util.Alertas;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.VBox;
 
 public class MainViewController implements Initializable{
 	
@@ -16,7 +25,7 @@ public class MainViewController implements Initializable{
 	private MenuItem menuItemDepartamento;
 	
 	@FXML
-	private MenuItem menuItemAbout;
+	private MenuItem menuItemAjuda;
 	
 	@FXML
 	public void onMenuItemVendedorAction() {
@@ -29,12 +38,36 @@ public class MainViewController implements Initializable{
 	}
 	
 	@FXML
-	public void onMenuItemAboutAction() {
-		System.out.println("onMenuItemAboutAction");
+	public void onMenuItemAjudaAction() {
+		loadView("/gui/Ajuda.fxml");
 	}
 	
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
+	}
+	
+	private synchronized void loadView(String nomeAbsoluto) { // Função para abrir uma outra tela. synchronized = Garante que o processamento não será interrompido pelo Thread
+		try {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto)); // Padrão do método ' (getClass().getResource(nomeAbsoluto)) '
+			VBox newVBox = loader.load();
+			
+			Scene mainScene = Main.getMainScene();
+			VBox mainVBox =(VBox) ((ScrollPane)mainScene.getRoot()).getContent();
+			
+			Node mainMenu = mainVBox.getChildren().get(0);
+			
+			mainVBox.getChildren().clear();
+			mainVBox.getChildren().add(mainMenu);
+			mainVBox.getChildren().addAll(newVBox.getChildren());
+			
+			/*
+			 * Com isso é possível manipular a cena principal
+			 * e incluir o Main Menu e os filhos da janela que está sendo aberta pelo método.
+			 */
+		}
+		catch (IOException e) {
+			Alertas.showAlert("IO Exception", "Erro carregando a página.", e.getMessage(), AlertType.ERROR);
+		}
 	}
 
 }
