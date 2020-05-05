bab897c139f7fc496d1c1536cec1baf8d461534c
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-05-05 11:04:14 -0300

    Implementando DepartamentoFormController

---
+++ src/gui/DepartamentoFormControler.java
@@ -0,0 +1,49 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Restricoes;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartamentoFormControler implements Initializable{
+	
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField txtNome;
+	
+	@FXML
+	private Label labelErrorNome;
+	
+	@FXML
+	private Button btSalvar;
+	
+	@FXML
+	private Button btCancelar;
+	
+	@FXML
+	public void onBtSalvarAction() {
+		System.out.println("onBtSalvarAction");
+	}
+	
+	@FXML
+	public void onBtCancelarAction() {
+		System.out.println("onBtCancelarAction");
+	}
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		initializeNodes();
+	}
+
+	private void initializeNodes() {
+		Restricoes.setTextFieldInteger(txtId);
+		Restricoes.setTextFieldMaxLength(txtNome, 30);
+	}
+}
