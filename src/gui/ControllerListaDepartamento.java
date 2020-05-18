d5f8b8f3b87cc96c78b5af37c2412300816a1320
Ronaldo Gaston <ronaldogaston@outlook.com>
2020-05-18 15:44:39 -0300

    Atualizando um departamento

--- src/gui/ControllerListaDepartamento.java
+++ src/gui/ControllerListaDepartamento.java
@@ -9,6 +9,7 @@ import application.Main;
 import gui.ouvintes.AtualizaDadosLista;
 import gui.util.Alertas;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -18,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -27,53 +29,67 @@ import javafx.stage.Stage;
 import model.entities.Departamento;
 import model.service.ServicoDepartamento;
 
-public class ControllerListaDepartamento implements Initializable, AtualizaDadosLista{
-	
+
+public class ControllerListaDepartamento implements Initializable, AtualizaDadosLista {
+
 	private ServicoDepartamento service; // Injetar a depeência sem colocar a implementação 'new ServicoDepartamento'
-	
+
 	@FXML
 	private TableView<Departamento> tableViewDepartamento; // Tipo TableView
-	
+
 	@FXML
-	private TableColumn<Departamento, Integer> colunaId; // Tipo Coluna. OBS: Lembrando que só declarar o mesmo não faz com que funcione. Verifique o método 'initialize (URL uri, ResourceBundle rb)'
-	
+	private TableColumn<Departamento, Integer> colunaId; // Tipo Coluna. OBS: Lembrando que só declarar o mesmo não faz
+															// com que funcione. Verifique o método 'initialize (URL
+															// uri, ResourceBundle rb)'
+
 	@FXML
-	private TableColumn<Departamento, Integer> colunaNome; // Tipo Coluna. OBS: Lembrando que só declarar o mesmo não faz com que funcione. Verifique o método 'initialize (URL uri, ResourceBundle rb)'
-	
+	private TableColumn<Departamento, Integer> colunaNome; // Tipo Coluna. OBS: Lembrando que só declarar o mesmo não
+															// faz com que funcione. Verifique o método 'initialize (URL
+															// uri, ResourceBundle rb)'
+
+	@FXML
+	private TableColumn<Departamento, Departamento> colunaEditar; // Tipo Coluna. OBS: Lembrando que só declarar o mesmo
+																	// não faz com que funcione. Verifique o método
+																	// 'initialize (URL uri, ResourceBundle rb)'
+
 	@FXML
 	private Button btNew; // Tipo Botão
-	
+
 	private ObservableList<Departamento> obsList;
-	
+
 	@FXML
-	public void onBtNewAction(ActionEvent event) { // Ação que ocorrerá após o botão 'Novo' ser clicado -- (ActionEvent event) Para ter referencia do controle que receber o evento
+	public void onBtNewAction(ActionEvent event) { // Ação que ocorrerá após o botão 'Novo' ser clicado -- (ActionEvent
+													// event) Para ter referencia do controle que receber o evento
 		Stage parentStage = Utils.currentStage(event);
 		Departamento dep = new Departamento();
-		cadastroDialogoFormulario(dep, "/gui/DepartamentoForm.fxml", parentStage); // Chamada do método de formulário de cadastro
+		cadastroDialogoFormulario(dep, "/gui/DepartamentoForm.fxml", parentStage); // Chamada do método de formulário de
+																					// cadastro
 	}
-	
+
 	public void setServicoDepartamento(ServicoDepartamento service) {
 		this.service = service;
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
-		InitializeNodes(); // Macete para funcionar um componente da tela. Veja o método 'InitializeNodes()'
+		InitializeNodes(); // Macete para funcionar um componente da tela. Veja o método
+							// 'InitializeNodes()'
 	}
 
 	private void InitializeNodes() {
-		colunaId.setCellValueFactory(new PropertyValueFactory<>("id")); // Comando para iniciar apropriadamento o comportamento da coluna na tabela
-		colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome")); // Comando para iniciar apropriadamento o comportamento da coluna na tabela
-		
+		colunaId.setCellValueFactory(new PropertyValueFactory<>("id")); // Comando para iniciar apropriadamento o
+																		// comportamento da coluna na tabela
+		colunaNome.setCellValueFactory(new PropertyValueFactory<>("nome")); // Comando para iniciar apropriadamento o
+																			// comportamento da coluna na tabela
+
 		/*
-		 * Comando para a tabela acompanhar a altura da janela
-		 * ABAIXO 
+		 * Comando para a tabela acompanhar a altura da janela ABAIXO
 		 */
-		
+
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
 	}
-	
+
 	public void updateTableView() {
 		if (service == null) {
 			throw new IllegalStateException("Serviço está nulo.");
@@ -81,34 +97,38 @@ public class ControllerListaDepartamento implements Initializable, AtualizaDados
 		List<Departamento> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
+		inicBotaoEditar();
 	}
 
-	private void cadastroDialogoFormulario(Departamento dep, String nomeAbsoluto, Stage parentStage) { // Janela de Diálogo
+	private void cadastroDialogoFormulario(Departamento dep, String nomeAbsoluto, Stage parentStage) { // Janela de
+																										// Diálogo
 		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto)); // Padrão do método ' (getClass().getResource(nomeAbsoluto)) '
+			FXMLLoader loader = new FXMLLoader(getClass().getResource(nomeAbsoluto)); // Padrão do método '
+																						// (getClass().getResource(nomeAbsoluto))
+																						// '
 			Pane pane = loader.load();
-			
+
 			DepartamentoFormController controller = loader.getController();
 			controller.setDepartamento(dep);
 			controller.setServicoDepartamento(new ServicoDepartamento());
 			controller.sobrescreveAtualizaDadosLista(this); // Cadeia de chamadas até o método 'onAtualizaDados'
 			controller.updateDados();
-			
+
 			Stage dialogoStage = new Stage();
-			
+
 			dialogoStage.setTitle("Informe os dados do departamento");
 			dialogoStage.setScene(new Scene(pane)); // Chamada de nova janela (janela filho) que irá sobrepor a anterior
 			dialogoStage.setResizable(false); // Faz com que a tela NÃO possa ser máximizada/minimizada (Redimencionada)
 			dialogoStage.initOwner(parentStage); // Chamada da janela pai da janela filho
-			dialogoStage.initModality(Modality.WINDOW_MODAL); // Bloqueia o acesso as telas de fundos até que janela filho tenha sido finalizada com sucesso
-			dialogoStage.showAndWait(); 
-			
+			dialogoStage.initModality(Modality.WINDOW_MODAL); // Bloqueia o acesso as telas de fundos até que janela
+																// filho tenha sido finalizada com sucesso
+			dialogoStage.showAndWait();
+
 			/*
-			 * Função para chamar a Janela do formulário de dialogo
-			 * Para preencher o novo departamento
+			 * Função para chamar a Janela do formulário de dialogo Para preencher o novo
+			 * departamento
 			 */
-		} 
-		catch (IOException e) {
+		} catch (IOException e) {
 			Alertas.showAlert("IO Exception", "Erro ao carrega janela", e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -117,5 +137,23 @@ public class ControllerListaDepartamento implements Initializable, AtualizaDados
 	public void onAtualizaDados() {
 		updateTableView();
 	}
-	
+
+	private void inicBotaoEditar() {
+		colunaEditar.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		colunaEditar.setCellFactory(param -> new TableCell<Departamento, Departamento>() {
+			private final Button button = new Button("Editar");
+
+			@Override
+			protected void updateItem(Departamento obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> cadastroDialogoFormulario(obj, "/gui/DepartamentoForm.fxml", Utils.currentStage(event)));
+			}
+		});
+	}
 }
